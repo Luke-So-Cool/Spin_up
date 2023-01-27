2855a055c002843b327610e31fcfb418fca2c853
Apple-Kong <kongthn@gmail.com>
2023-01-27 16:26:52 +0900

    feat result View show

--- SpinUp/SpinUp/ContentViewModel.swift
+++ SpinUp/SpinUp/ContentViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 송시원 on 2023/01/27.
 //
 
+import Combine
 import SwiftUI
 
 enum ViewMode {
@@ -12,6 +13,8 @@ enum ViewMode {
 }
 
 class ContentViewModel: ObservableObject {
+    
+    private var cancellables = Set<AnyCancellable>()
 
     @Published var viewState: ViewMode = .main
     @Published var isShowingShopView: Bool = false
@@ -19,12 +22,23 @@ class ContentViewModel: ObservableObject {
     @Published var bestScore: Int = 30000
     @Published var gemPoint: Int = 3000
 
-    @Published var gameManager = GameManager.shared
+    @State var gameManager = GameManager.shared
     //어떤 역할로 쓸까? -> 일단 가져가서 사용되어야할텐데...
     func didTabSpinButton() {
         viewState = .play
         GameManager.shared.state = .running
 
+    }
+    
+    init() {
+        gameManager.$state
+            .removeDuplicates()
+            .sink { newState in
+                if newState == .stop && self.gameManager.lastState == .running {
+                    self.viewState = .result
+                }
+            }
+            .store(in: &cancellables)
     }
 
 }
